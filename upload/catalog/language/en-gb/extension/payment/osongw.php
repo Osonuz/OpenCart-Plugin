b4e211ce8b4a3a304fde0cd0b125b5e12cc77634
Zafarbek Ikromov <zafarbek2225@gmail.com>
2021-11-15 10:38:26 +0500

    change name

--- upload/catalog/language/en-gb/extension/payment/osongw.php
+++ upload/catalog/language/en-gb/extension/payment/osongw.php
@@ -1,5 +1,5 @@
 <?php
-$_['text_title']  = 'Oson Payment Gateway';
+$_['text_title']  = 'OSON';
 $_['token_error'] = 'Error to create a payment token. Contact the store owner';
 $_['text_order']  = 'Order #';
 ?>
